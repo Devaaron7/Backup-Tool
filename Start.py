49e15eba5727e00ee3128a2b3f87ca382fcf23a2
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-12-23 17:09:23 -0500

    Add buttons & functions

--- Start.py
+++ Start.py
@@ -1,8 +1,18 @@
-from guizero import App, Box, PushButton
-
+from guizero import App, Box, PushButton, TextBox
+from tkinter.filedialog import askopenfilenames, askdirectory
 
 
 def main():
+    
+    
+    
+    def add_folder():
+        folder_selection = str((askdirectory(title="Select a Folder") + "/"))
+        dir_1.value = folder_selection
+        dir_2.show()
+        #dir_2.disable()
+         
+
     
     app = App(title="Backup Tool", width=500, height=250, bg="white")
     
@@ -10,7 +20,28 @@ def main():
     
     choose_hdd = PushButton(app, text="Select Backup Destination", align="left")
     
-    select_dir = PushButton(app, align="right")
+    dir_1 = TextBox(app, text="")
+    dir_1.disable()
+    
+    dir_2 = TextBox(app, text="test")
+    dir_2.disable()
+    dir_2.hide()
+    
+    dir_3 = TextBox(app, text="test")
+    dir_3.disable()
+    dir_3.hide()
+    
+    dir_4 = TextBox(app, text="test")
+    dir_4.disable()
+    dir_4.hide()
+    
+    dir_5 = TextBox(app, text="test")
+    dir_5.disable()
+    dir_5.hide()
+    
+    select_dir_1 = PushButton(app, image="./src/plus.png", command=add_folder)
+    
+    #add_dir = PushButton(app, image="./src/plus.png")
     
     app.display()
     
