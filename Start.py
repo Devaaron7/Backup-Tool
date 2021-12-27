938d25b3802f8000e4b3413c7305ab89a2ef98e7
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-12-27 08:51:52 -0500

    Working to understand layout better

--- Start.py
+++ Start.py
@@ -84,24 +84,53 @@ def main():
     
     
     #Gui Frame
-    app = App(title="Backup Tool", width=500, height=250, bg="white", layout="grid")
-    #box_left = Box(app, layout="grid")
-    box_right = Box(app, align="right", grid=[8,0])
+    #app = App(title="Backup Tool", width=500, height=250, bg="white", layout="grid")
+    app = App(title="Backup Tool", width=500, height=250, bg="white")
+    box_top_row = Box(app, width="fill", height="fill",  align="top")
+    box_bottom_row = Box(app, width="fill", height="fill",  align="bottom")
+    box_left = Box(box_top_row, width="fill", height="fill",  align="left")
+    box_center = Box(box_top_row, width="fill", height="fill", align="right")
+    box_right = Box(box_top_row, width="fill", height="fill", align="right")
+    box_bottom_status = Box(box_bottom_row, width="fill", align="bottom")
+    box_bottom_progress = Box(box_bottom_row, width="fill", align="bottom")
     
+    
+    button1 = PushButton(box_left, text="1", height="fill", width="fill")
+
+    button2 = PushButton(box_center, text="2", height="fill", width="fill")
+
+    button3 = PushButton(box_right, text="3", height="fill", width="fill")
+    
+    button4 = PushButton(box_bottom_status, text="4", width="fill")
+    
+    button5 = PushButton(box_bottom_progress, width="fill", text="5")
+    
+    
+    
+    '''
     #test1 = PushButton(app, grid=[0,0])
     #test2 = PushButton(app, grid=[3,0])
     
     
-    #button1 = PushButton(app, text="1", grid=[0,0], align="left")
-    #button2 = PushButton(app, text="2", grid=[0,1], align="left")
-    #button3  = PushButton(app, text="3", grid=[0,2], align="left")
-    #button4  = PushButton(box_right, text="4", grid=[10,0])
-    #button5  = PushButton(box_right, text="5", grid=[11,0])
-    #button6  = PushButton(box_right, text="6", grid=[12,0])
-    '''
-    button7  = PushButton(app, text="7", grid=[6,0])
-    button8  = PushButton(app, text="8", grid=[7,0])
-    button9  = PushButton(app, text="9", grid=[8,0])
+    #button1 = PushButton(app, text="1", align="left", grid=[0,0])
+    #button2 = PushButton(app, text="2", width=17, grid=[1,0])
+    #button3  = PushButton(app, text="3", align="right", grid=[2,0])
+    #button4  = PushButton(app, text="4", grid=[0,1])
+    #button5  = PushButton(app, text="5", grid=[1,1])
+    #button6  = PushButton(app, text="6", grid=[2,1])
+    #Button to add device where folders will be copied to
+    hdd = Picture(app, image="./src/folder.png", grid=[0,0])
+    hdd.hide()
+    hdd_text = Text(app, text="folder_selection", grid=[0,1])
+    hdd_text.hide()
+    choose_hdd = PushButton(app, text="Select Backup Destination", grid=[0,2], command=load_show_save_device)
+    
+    
+    
+    #button7  = PushButton(app, text="7", grid=[0,2])
+    #button8  = PushButton(app, text="8", grid=[1,2])
+    #button9  = PushButton(app, text="9", grid=[2,2])
+    
     button0  = PushButton(app, text="10", grid=[9,0])
     button0  = PushButton(app, text="11", grid=[10,0])
     button0  = PushButton(app, text="12", grid=[11,0])
@@ -112,22 +141,22 @@ def main():
     
     
     
-    '''
+    
     #Button to add device where folders will be copied to
-    hdd = Picture(app, image="./src/folder.png", grid=[0,0])
+    hdd = Picture(box_left, image="./src/folder.png", align="top")
     hdd.hide()
-    hdd_text = Text(app, text="folder_selection", grid=[0,1])
+    hdd_text = Text(box_left, text="folder_selection", align="top")
     hdd_text.hide()
-    choose_hdd = PushButton(app, text="Select Backup Destination", grid=[0,2], command=load_show_save_device)
-    '''
+    choose_hdd = PushButton(box_left, text="Select Backup Destination", align="top", command=load_show_save_device)
+    
    
     
     
     #Display of folder paths in the GUI - Programming 5 folders max
-    dir_1 = TextBox(box_right, text="", width=20, grid=[0,0])
+    dir_1 = TextBox(box_right, text="", width=20, align="right", grid=[0,0])
     dir_1.disable()
     
-    dir_2 = TextBox(box_right, text="", width=20, grid=[3,1])
+    dir_2 = TextBox(box_right, text="", width=20, grid=[0,1])
     dir_2.disable()
     dir_2.hide()
     
@@ -144,7 +173,7 @@ def main():
     dir_5.hide()
     
     #Button to add folders to copy list
-    select_dir_1 = PushButton(box_right, image="./src/plus.png", command=add_folder1)
+    select_dir_1 = PushButton(box_right, image="./src/plus.png", command=add_folder1, grid=[0,2])
     
     
     '''
